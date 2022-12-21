3a4d16af8bff3582a22e11a250426f2e64a36271
Isaiah <isaiah03@stanford.edu>
2022-12-21 00:06:33 +0000

    Update 13 files

--- src/pages/astro.config.mjs
+++ src/pages/astro.config.mjs
@@ -1,9 +1,5 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import image from "@astrojs/image";
-
-// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
